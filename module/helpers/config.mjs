02631c7f86cafc252d3918a0d9acaa3ec20dfaab
Luis Belloch <luis.bellochmartinez@gmail.com>
2024-04-29 23:36:23 +0200

    FEAT: starting boilerplate

---
+++ module/helpers/config.mjs
@@ -0,0 +1,19 @@
+export const RYUUTAMA = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+RYUUTAMA.abilities = {
+  str: 'RYUUTAMA.Ability.Str.long',
+  dex: 'RYUUTAMA.Ability.Dex.long',
+  int: 'RYUUTAMA.Ability.Int.long',
+  spi: 'RYUUTAMA.Ability.Spi.long',
+};
+
+RYUUTAMA.abilityAbbreviations = {
+  str: 'RYUUTAMA.Ability.Str.abbr',
+  dex: 'RYUUTAMA.Ability.Dex.abbr',
+  int: 'RYUUTAMA.Ability.Int.abbr',
+  spi: 'RYUUTAMA.Ability.Spi.abbr',
+};
