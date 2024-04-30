ad71c6dc50febfb6f86ee394aa664c860487127b
Luis Belloch <luis.bellochmartinez@gmail.com>
2024-04-30 21:40:00 +0200

    FEAT: attack rolls and damage

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -30,7 +30,7 @@ export class RyuutamaItem extends Item {
   }
 
   /**
-   * Handle clickable rolls.
+   * Handle clickable rolls from items.
    * @param {Event} event   The originating click event
    * @private
    */
@@ -40,10 +40,10 @@ export class RyuutamaItem extends Item {
     // Initialize chat data.
     const speaker = ChatMessage.getSpeaker({ actor: this.actor });
     const rollMode = game.settings.get('core', 'rollMode');
-    const label = `[${item.type}] ${item.name}`;
 
-    // If there's no roll data, send a chat message.
-    if (!this.system.formula) {
+    // If there's no attack formula, send a chat message.
+    if (!this.system.attack_formula) {
+      const label = `<h2>${item.name}</h2>`;
       ChatMessage.create({
         speaker: speaker,
         rollMode: rollMode,
@@ -55,17 +55,26 @@ export class RyuutamaItem extends Item {
     else {
       // Retrieve roll data.
       const rollData = this.getRollData();
-
+      const attack_label = `<h2>${this.actor.name} ${game.i18n.localize('RYUUTAMA.Item.Weapon.AttacksWith')} ${item.name}</h2>`;
+      let attack_roll_string = `d${this.actor.system.abilities[rollData.attack_formula.roll1].value}+d${this.actor.system.abilities[rollData.attack_formula.roll2].value}+${rollData.attack_formula.diceBonus}`
+      const attack_roll = new Roll(attack_roll_string, rollData);
+      const damage_label = `<h2>${item.name} ${game.i18n.localize('RYUUTAMA.Item.Weapon.DamageDealt')}</h2>`;
+      let damage_roll_string = `d${this.actor.system.abilities[rollData.damage_formula.roll1].value}+${rollData.damage_formula.diceBonus}`
+      const damage_roll = new Roll(damage_roll_string, rollData);
       // Invoke the roll and submit it to chat.
-      const roll = new Roll(rollData.formula, rollData);
-      // If you need to store the value first, uncomment the next line.
-      // const result = await roll.evaluate();
-      roll.toMessage({
+      await damage_roll.evaluate()
+      await attack_roll.evaluate()
+      attack_roll.toMessage({
         speaker: speaker,
         rollMode: rollMode,
-        flavor: label,
-      });
-      return roll;
+        flavor: attack_label,
+      })
+      damage_roll.toMessage({
+        speaker: speaker,
+        rollMode: rollMode,
+        flavor: damage_label,
+      })
+      return;
     }
   }
 }
